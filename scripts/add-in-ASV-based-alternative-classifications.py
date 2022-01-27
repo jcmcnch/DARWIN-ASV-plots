34592ebd3530057f3967f0d0aac5cdceaf4ea75f
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2022-01-26 18:27:12 -0800

    Fix bug with ASVid-based classifcations.

--- scripts/add-in-ASV-based-alternative-classifications.py
+++ scripts/add-in-ASV-based-alternative-classifications.py
@@ -7,12 +7,11 @@ import csv
 #create empty hash
 hashClassifications = {}
 
-#Read in previous classifications (e.g. from a previous dataset)
-#Format requirements: tab-separated values with following columns:
-#OTU_ID taxonomy	alternative classification and confidence	model_category	approx_size (ref)
+#Read in previous ASV-based classifications (e.g. from comparison to ProPortal genomes)
+#Format requirements: tab-separated values with following columns (no headers):
+#OTU_ID 	classification
 for astrLine in csv.reader(open(sys.argv[1]), csv.excel_tab):
-	if astrLine[1] != "taxonomy":
-		hashClassifications[astrLine[0]] = astrLine[2:]
+    hashClassifications[astrLine[0]] = astrLine[1]
 
 #Read in a taxonomically-annotated ASV table (tab-separated format)
 #that has NOT been classified with model-relevant annotations
@@ -25,12 +24,14 @@ aTaxa = list(hashClassifications.keys())
 	
 for astrLine in csv.reader(open(sys.argv[2]), csv.excel_tab):
     query = astrLine[0].strip()
+    #if ASV id matches
     if query in aTaxa:
-        aOut = [astrLine[0], astrLine[1]] + hashClassifications[query] + astrLine[2:]
+        #print replace the 3rd column with the associated classification
+        aOut = [astrLine[0], astrLine[1], hashClassifications[query]] + astrLine[3:]
         print('\t'.join(aOut))
+    #do nothing if it's the first line
     elif astrLine[1] == "taxonomy":
-        aOut = [astrLine[0]] + [astrLine[1], "alternative classification and confidence", "model category", "approximate size (ref)"] + astrLine[2:]
-        print('\t'.join(aOut))
+        print('\t'.join(astrLine[0:]))
+    #or if the ASV id doesn't match the dictionary
     else:
-        aOut = [astrLine[0]] + [astrLine[1], "", "", ""] + astrLine[2:]
-        print('\t'.join(aOut))
+        print('\t'.join(astrLine[0:]))
